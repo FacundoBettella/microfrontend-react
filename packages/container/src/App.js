0ef3989bc11f7a0f81801c5290993ac96207b671
facundobettella <facundobettella@outlook.com>
2023-04-08 17:12:28 -0300

    Finished feature #1

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import { MarketingApp } from "./components/MarketingApp";
 export const App = () => {
   return (
     <Fragment>
-      <h2>Container</h2>
+      <h2>We are at the container repository of my microfrontend</h2>
       <hr />
       <MarketingApp />
     </Fragment>
